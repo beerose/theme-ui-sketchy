179162575ec2c59a12baa3a0c1c5d154a18fde56
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-04-27 22:29:56 +0200

    Update components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const ThemeJson = () => {
       sx={{
         width: "100%",
         fontFamily: "monospace",
-        bg: "muted",
+        bg: "highlight",
         p: 2,
         border: 0,
         borderRadius: "sketchy3",
